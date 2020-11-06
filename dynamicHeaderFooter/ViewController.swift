71ccbf0ba922ef0ab63a897b37545d788c581282
spramani <shubhamramani903@gmail.com>
2020-11-06 10:20:35 +0530

    Initial Commit

---
+++ dynamicHeaderFooter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dynamicHeaderFooter
+//
+//  Created by MAC on 06/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
